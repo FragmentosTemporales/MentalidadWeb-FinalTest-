f1dcc3d61464ca505fa5bfd455305a47dd318657
FragmentosTemporales <cristian.rivera3284@gmail.com>
2023-07-20 17:27:58 -0400

    Actualización e implementación de test

--- backend/manage.py
+++ backend/manage.py
@@ -33,5 +33,20 @@ def create_user(username, email, password):
         raise e
 
 
+@cli.command("test")
+@click.option("--test_name")
+def test(test_name=None):
+    """ Runs the unit tests."""
+    import unittest
+    if test_name is None:
+        tests = unittest.TestLoader().discover('tests', pattern="test*.py")
+    else:
+        tests = unittest.TestLoader().loadTestsFromName('tests.' + test_name)
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
+
+
 if __name__ == "__main__":
     cli()
