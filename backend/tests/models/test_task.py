666ddefef2dee1344a4ceab8cbed0abadc536d94
Trinidad Marti Gutierrez <trini@MacBook-Air-de-Trinidad.local>
2023-07-21 14:50:51 -0400

    test_create_task working

--- backend/tests/models/test_task.py
+++ backend/tests/models/test_task.py
@@ -1,7 +1,7 @@
 import unittest
 from app.models import Task
 from tests import BaseTestCase
-from tests.utils.user import save_user_to_db
+from tests.utils.task import save_task_to_db
 
 class TestTaskModel(BaseTestCase):
     """ Test that task model is ok """
@@ -10,13 +10,22 @@ class TestTaskModel(BaseTestCase):
         """ Setting up the test class  """
         super().setUp()
         self.data = {
-            "title":"prueba",
+            "task":"prueba",
             "description":"prueba",
+            "is_completed": False,
             "user_id": 1
         }
     
-    def task_exist(self):
-      
+    def test_create_task(self):
+        """ Test create task is success """
+        task = save_task_to_db(self.data)
+        self.assertIsNotNone(task.id)
+        for key in self.data.keys():
+            self.assertEqual(
+                getattr(task, key), self.data.get(key, None) 
+            )
+
+    "filtrar id, tareas de usuario, borrar tareas, "
 
 if __name__ == "__main__":
     unittest.main()
